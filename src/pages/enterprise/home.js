6e5a35779830491d54e1ff18fa58ceec64f2b787
melgs08 <andryasilvestre@gmail.com>
2022-12-11 15:20:54 -0300

    tela home: 85%

--- src/pages/enterprise/home.js
+++ src/pages/enterprise/home.js
@@ -1,97 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Dimensions } from 'react-native';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export default function Home(navigation) {
     return(
-
         <View style = {styles.body}>
             <Text style = {styles.mainTexts}> Visão Geral</Text>
             <View style = {styles.good}>
-                <View style = { styles.healthyMachines}>
+                <View style = { styles.firstSet}>
                     {/* <Button styles= {styles.buttons1} onPress={() => navigation.navigate('ListMachine')}> */}
-                    <div className="buttons1" style={{background: "linear-gradient(#e66465, #9198e5);" }}>
-                        <Text style= {styles.textBt}>
-                            FUNCIONANDO
-                        </Text>
-                        <Text style= {styles.textBt}>
-                            PERFEITAMENTE
-                        </Text> 
-                    </div>
-
-
-                        
-                    {/* </Button>  */}
+                    <View style={styles.row}>
+                        <View style = {styles.healthyMc}>  
+                            <button style = {styles.firstSetBt} type="">  
+                                <View style= {styles.amountText}>
+                                    <Text style= {styles.textBt}>FUNCIONANDO</Text>
+                                    <Text style= {styles.textBt}>NORMALMENTE</Text> 
+                                    <Text style= {styles.amountText}>2</Text> 
+                                </View>
+                            </button>
+                        </View>
+                    <button style = {styles.secSetBt} type="">  
+                        <View style= {styles.amountText}>
+                            <Text style= {styles.textBt}>FUNCIONANDO </Text>
+                            <Text style= {styles.textBt}>EM ALERTA</Text>
+                            <Text style= {styles.amountText}>0</Text> 
+                        </View> 
+                    </button>
+                    </View>  
                 </View> 
-                <Text style= {styles.textBt}>
-                    FUNCIONANDO
-                </Text>
-                <Text style= {styles.textBt}>
-                    PERFEITAMENTE
-                </Text>
-                {/* inserir text com a api, post no numero */}
-
-                <Text style= {styles.textBt}>
-                    FUNCIONANDO
-                </Text>
-                <Text style= {styles.textBt}>
-                    EM ALERTA
-                </Text>
-                <Text style= {styles.textBt}>
-                    EM ESTADO
-                </Text>
-                <Text style= {styles.textBt}>
-                    CRÍTICO
-                </Text>
-                <Text style= {styles.textBt}>
-                    TODAS AS
-                </Text>
-                <Text style= {styles.textBt}>
-                    MÁQUINAS
-                </Text>
+                <View style = { styles.firstSet}>
+                    {/* <Button styles= {styles.buttons1} onPress={() => navigation.navigate('ListMachine')}> */}
+                    <View style={styles.row}>
+                        <View style = {styles.healthyMc}>  
+                            <button style = {styles.trdSetBt} type="">  
+                                <View style= {styles.amountText}>
+                                    <Text style= {styles.textBt}>EM ESTADO</Text>
+                                    <Text style= {styles.textBt}>CRÍTICO</Text> 
+                                    <Text style= {styles.amountText}>1</Text> 
+                                </View>
+                            </button>
+                        </View>
+                    <button style = {styles.fortSetBt} type="">  
+                        <View style= {styles.amountText}>
+                        <Text style= {styles.textBt}>TODAS AS </Text>
+                            <Text style= {styles.textBt}>MÁQUINAS </Text> 
+                            <Text style= {styles.amountText}>15</Text> 
+                        </View>
+                    </button>
+                    </View>  
+                </View> 
             </View>
-            <Text style = {styles.mainTexts}> Categorias </Text>
-            <View styles = {styles.selectMachine}>
-                <Text style= {styles.categories}>
-                    Motores
-                </Text>
-                <Text style= {styles.categories}>
-                    Injetoras
-                </Text>
-                <Text style= {styles.categories}>
-                    Sensores
-                </Text>
-                <Text style= {styles.categories}>
-                    Extratoras
-                </Text>
-                <Text style= {styles.categories}>
-                    Empilhadeiras
-                </Text>
+            <View style = {styles.catSpacing}>
+                <Text style = {styles.mainTexts}> Categorias </Text> 
             </View>
-
-        </View>
-
-       
+            <View style = {styles.categories}>
+                    <View style={styles.row}>
+                        <button style={styles.firstSetCatBt} type="">Motores</button>
+                        <button style={styles.firstSetCatBt} type="">Injetoras</button>
+                    </View>             
+                <View style={styles.row}>
+                    <button style={styles.secSetCatBt} type="">Sensores</button>
+                    <button style={styles.secSetCatBt}type="">Extratoras</button>
+                </View>
+                <View style={styles.row}>
+                    <button  style={styles.thirdSetCatBt} type="">Empilhadeiras</button>
+                    <button  style={styles.thirdSetCatBt} type="">Esteiras</button>
+                </View>
+            </View>        
+        </View>   
     );
 }
 
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 'none',
         backgroundColor: '#fff',
         flexGrow: 1,
+        width: screenWidth,
+        height: screenHeight,
     },
     body: {
-        marginLeft: '5%'
+        justifyContent: 'space-evenly',
+        marginLeft: '3%',
+        fontFamily: 'Montserrat',
+
     },
 
     mainTexts: {
         marginTop: '5%',
         fontWeight: 'bold', 
         fontSize: '25px',
+        color: '#212529',
         fontFamily: 'Calibri',
+
+        
 
     },
     header: {
@@ -107,17 +113,161 @@ const styles = StyleSheet.create({
 
 
     },
+    amountText: {
+        fontSize: '25px',
+        fontWeight: 'bold',
+        fontFamily: 'Helvetica',
+        marginTop: '3%',
+        textAlign: 'justify',
+        marginLeft: '2%'
+
+
+    },
+    catSpacing: {
+        marginTop: '2%'
+    },
+
     textBt:{
         fontSize: '18px',
         fontFamily: 'Calibri',
-        marginLeft: '7%',
+        marginLeft: '2%'
 
     },    
     categories: {
         fontFamily: 'Calibri',
         fontSize: '18px',
-        marginLeft: '15%'
+        marginTop: '4%',
+        justifyContent: 'space-evenly'
 
-    }
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'left',
+    },
+    firstSetBt: {
+        backgroundColor: 'white',
+        minWidth: '115px',
+        width: '210px',
+        height: '118px',
+        borderLeftColor: '#0D9B5B',
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderRadius: '10px',
+        borderLeftWidth: '5px',
+        marginTop:'30px',
+        marginLeft: '8%',
+        textAlign: 'justify'
+        
+    },
+    secSetBt: {
+        backgroundColor: 'white',
+        minWidth: '115px',
+        width: '210px',
+        height: '118px',
+        borderLeftColor: '#F6C23E',
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderRadius: '10px',
+        borderLeftWidth: '5px',
+        marginTop:'30px',
+        marginLeft: '8%',
+        textAlign: 'justify'
 
+
+        
+    },
+    trdSetBt: {
+        backgroundColor: 'white',
+        minWidth: '115px',
+        width: '210px',
+        height: '118px',
+        borderLeftColor: '#E74A3B',
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderRadius: '10px',
+        borderLeftWidth: '5px',
+        marginTop:'30px',
+        marginLeft: '8%',
+        textAlign: 'justify',
+        
+
+
+    },
+
+    fortSetBt: {
+        backgroundColor: 'white',
+        minWidth: '115px',
+        width: '210px',
+        height: '118px',
+        borderLeftColor: '#D2D2D2',
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderRadius: '10px',
+        borderLeftWidth: '5px',
+        marginTop:'30px',
+        marginLeft: '8%',
+        textAlign: 'justify',
+
+    },
+
+    firstSetCatBt: {
+        backgroundColor: '#09427D',
+        border: 'none',
+        height: '56px',
+        textAlign: 'center',
+        borderRadius: '10px',
+        color: 'white',
+        marginBottom: '2%',
+        marginLeft: '5%',
+        minWidth: '118px',
+        fontSize: '20px',
+        width: '200px',
+        marginTop: '1%'
+
+    },
+    
+    secSetCatBt: {
+        backgroundColor: '#09427D',
+        border: 'none',
+        height: '56px',
+        textAlign: 'center',
+        borderRadius: '10px',
+        color: 'white',
+        marginBottom: '2%',
+        marginLeft: '5%',
+        minWidth: '118px',
+        maxWidth: '225px',
+        fontSize: '20px',
+        width: '200px',
+        marginTop: '2%'
+
+
+
+    },
+
+    thirdSetCatBt: {
+        backgroundColor: '#09427D',
+        border: 'none',
+        width: '200px',
+        height: '56px',
+        textAlign: 'center',
+        borderRadius: '10px',
+        color: 'white',
+        marginBottom: '2%',
+        marginLeft: '5%',
+        minWidth: '118px',
+        fontSize: '20px',
+        marginTop: '2%'
+
+
+    },
+
+
+
+    
 });
